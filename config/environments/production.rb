2ff92ecc08d113df42f4c51ceedbf2cf0baf5019
jakubkriz <jakubkriz92@gmail.com>
2015-12-05 14:49:04 +0100

    [MOD] letter_opener_web on production - testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { host: 'http://fierce-plains-8000.herokuapp.com' }
+  config.action_mailer.delivery_method = :letter_opener_web
   # config.action_mailer.delivery_method = :letter_opener
 
   # Code is not reloaded between requests.
