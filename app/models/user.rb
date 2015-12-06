0b00c9fa4d367d018dea91f00538c46b5c9e5295
jakubkriz <jakubkriz92@gmail.com>
2015-12-06 10:56:03 +0100

    [MOD] user's posts are deleted when his account is cancelled

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :posts
+  has_many :posts, dependent: :destroy
 
 
 has_attached_file :avatar,
